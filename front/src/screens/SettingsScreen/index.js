959e05dbe84004c5ab4832cda03af29e257d3658
kimsoyeong <soyeong.kim9@gmail.com>
2024-05-18 00:27:22 +0900

    design: Create SettingScreen

---
+++ front/src/screens/SettingsScreen/index.js
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+function SettingScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ textAlign: "center" }}>Setting Screen</Text>
+    </View>
+  );
+}
+
+export default SettingScreen;
