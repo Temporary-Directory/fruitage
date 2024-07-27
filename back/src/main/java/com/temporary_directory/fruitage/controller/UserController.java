5cbad2664bf26f4d3fcdc4f39abdba031cf6f7d1
Parksohui <psh990525@naver.com>
2024-07-27 21:37:03 +0900

    feat: Get userName

--- back/src/main/java/com/temporary_directory/fruitage/controller/UserController.java
+++ back/src/main/java/com/temporary_directory/fruitage/controller/UserController.java
@@ -6,6 +6,7 @@ import com.temporary_directory.fruitage.dto.request.FruitRequestDTO;
 import com.temporary_directory.fruitage.dto.response.FruitInfoResponseDTO;
 import com.temporary_directory.fruitage.dto.response.UserFruitInfoResponseDTO;
 import com.temporary_directory.fruitage.dto.response.FruitResponseDTO;
+import com.temporary_directory.fruitage.dto.response.UserInfoResponseDTO;
 import com.temporary_directory.fruitage.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,40 +23,50 @@ public class UserController {
 
     // character
     @PostMapping("/character")
-    @ResponseStatus(code= HttpStatus.OK)
-    public void createCharacter(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody CharacterRequestDTO characterRequestDTO){
+    @ResponseStatus(code = HttpStatus.OK)
+    public void createCharacter(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody CharacterRequestDTO characterRequestDTO) {
         userService.createCharacter(principalDetails.getUser(), characterRequestDTO.getCharacterType());
     }
 
     @PutMapping("/character")
     @ResponseStatus(code = HttpStatus.OK)
-    public void updateCharacter(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody CharacterRequestDTO characterRequestDTO){
+    public void updateCharacter(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody CharacterRequestDTO characterRequestDTO) {
         userService.updateCharacter(principalDetails.getUser(), characterRequestDTO.getCharacterType());
     }
 
     // fruit
     @GetMapping("/fruit")
     @ResponseStatus(code = HttpStatus.OK)
-    public FruitResponseDTO getFruitCount(@AuthenticationPrincipal PrincipalDetails principalDetails){
+    public FruitResponseDTO getFruitCount(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return userService.getFruitCount(principalDetails.getUser());
     }
 
     @PostMapping("/fruit")
     @ResponseStatus(code = HttpStatus.OK)
-    public void selectFruit(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody FruitRequestDTO fruitRequestDTO){
+    public void selectFruit(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody FruitRequestDTO fruitRequestDTO) {
         userService.selectFruit(principalDetails.getUser(), fruitRequestDTO.getFruitIdList());
     }
 
     @GetMapping("/fruit/info")
     @ResponseStatus(code = HttpStatus.OK)
-    public List<UserFruitInfoResponseDTO> getFruitInfo(@AuthenticationPrincipal PrincipalDetails principalDetails){
+    public List<UserFruitInfoResponseDTO> getFruitInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return userService.getFruitInfo(principalDetails.getUser());
     }
 
     @GetMapping("/fruit/{fruitId}")
     @ResponseStatus(code = HttpStatus.OK)
-    public List<FruitInfoResponseDTO> getNewFruitInfo(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable("fruitId") int fruitId){
+    public List<FruitInfoResponseDTO> getNewFruitInfo(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable("fruitId") int fruitId) {
         return userService.getNewFruitInfo(principalDetails.getUser(), fruitId);
     }
 
+    // user
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public UserInfoResponseDTO getUserName(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestParam("flag") boolean flag) {
+        if (flag) {
+            return new UserInfoResponseDTO(principalDetails.getUser().getUserId(), principalDetails.getUser().getUserLoginName());
+        } else {
+            return new UserInfoResponseDTO(principalDetails.getUser().getUserId(), principalDetails.getUser().getUserName());
+        }
+    }
 }
---
+++ back/src/main/java/com/temporary_directory/fruitage/dto/response/UserInfoResponseDTO.java
@@ -0,0 +1,11 @@
+package com.temporary_directory.fruitage.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoResponseDTO {
+    private int userId;
+    private String userName;
+}
